0acf358ed4f47be81b5b3d890e671123cafa10f6
bellcha <bellcha@gmail.com>
2022-02-24 06:50:50 -0600

    spacing

--- wordle_solver.py
+++ wordle_solver.py
@@ -3,27 +3,31 @@ from letter_types import *
 with open('wordle_dict.txt', 'r') as f:
     words_ = f.read().splitlines()
 
-grey = ['u', 'l', 'k', 'r', 'n', 'h', 'f', 'e', 'd']
-yellow = []
-green = {0:'t',1:'a', 2:'c'}
-guesses = []
+grey = ['g', 'a', 'i', 's', 'c', 'r', 'a', 'n']
+yellow = ['b']
+green = {1:'l', 2:'o',4:'e'}
+guesses = ['nails', 'crane', 'globe']
 
 for word in words_:
     if len(grey) > 0 and len(yellow) > 0 and len(green) > 0:
         if not grey_letters(word, grey) and yellow_letters(word, yellow) and green_letters(word, green):
             if word not in guesses:
                 print(word)
+    
     elif len(grey) > 0 and len(yellow) > 0 and len(green) == 0:
         if not grey_letters(word, grey) and yellow_letters(word, yellow):
             if word not in guesses:
                 print(word)
+    
     elif len(yellow) > 0 and len(green) > 0 and len(grey) == 0:
         if yellow_letters(word, yellow) and green_letters(word, green):
             if word not in guesses:
                 print(word)
+    
     elif len(grey) > 0 and len(green) > 0 and len(yellow) == 0:
         if not grey_letters(word, grey) and green_letters(word, green):
             if word not in guesses:
                 print(word)
+    
     else:
         print('Minimum criteria not met for results')
