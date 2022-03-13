3769c73cbd9e01380da9e143feaafe6de16558e3
bellcha <bellcha@gmail.com>
2022-03-13 09:05:40 -0500

    more helpers

--- letter_types.py
+++ letter_types.py
@@ -1,14 +1,14 @@
-def grey_letters(word, letters) -> bool:
+def grey_letters(word: str, letters: list) -> bool:
     w = list(word)
     check =  any(letter in w for letter in letters)
     return check
 
-def yellow_letters(word, letters) -> bool:
+def yellow_letters(word: str, letters: list) -> bool:
     w = list(word)
     check =  all(letter in w for letter in letters)
     return check
 
-def green_letters(word, letters) -> bool:
+def green_letters(word: str, letters: dict) -> bool:
     check = []
     w = list(word)
     for key, value in letters.items():
