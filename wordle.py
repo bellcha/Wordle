2598de177806cb68c97907c651c9cc2629fb534a
bellcha <bellcha@gmail.com>
2022-03-26 22:03:38 -0500

    creating classes

--- wordle.py
+++ wordle.py
@@ -1,53 +1,79 @@
+from dataclasses import dataclass
+from wordledict import wordle_dict
 
-def grey_letters(word: str, letters: list) -> bool:
-    w = list(word)
-    check =  any(letter in w for letter in letters)
-    return check
 
-def yellow_letters(word: str, letters: list) -> bool:
-    w = list(word)
-    check =  all(letter in w for letter in letters)
-    return check
-
-def green_letters(word: str, letters: dict) -> bool:
-    check = []
-    w = list(word)
-    for key, value in letters.items():
-        if w[key] == value:
-            check.append(True)
-        else:
-            check.append(False)
+@dataclass
+class WordleTypes:
+    word: str
+    letters: list
+    @property
+    def grey_letters(self) -> bool:
+        w = list(self.word)
+        check =  any(letter in w for letter in self.letters)
+        return check
     
-    return all(check)
+    @property
+    def letter_check(self) -> bool:
+        w = list(self.word)
+        check =  all(letter in w for letter in self.letters)
+        return check
 
-def wordle(grey: list, yellow: list, green: dict, guesses: list):
+    @property
+    def green_check(self) -> bool:
+        check = []
+        w = list(self.word)
+        for key, value in self.letters.items():
+            if w[key] == value:
+                check.append(True)
+            else:
+                check.append(False)
+    
+        return all(check)
 
-    with open('wordle_dict.txt', 'r') as f:
-        _words = f.read().splitlines()
+@dataclass
+class WordleSolver(WordleTypes):
+    
+    _grey: list
+    _yellow: list
+    _green: list
+    
+    def letter_check(self):
+        w = list(self.word)
+        check =  all(letter in w for letter in self.letters)
+        return check
 
-    for word in _words:
+def wordle(grey: list, yellow: list, green: list, guesses: list):
+
+    for word in wordle_dict:
+
+        grey_letters = WordleTypes(word, grey)
+
+        yellow_letters = WordleTypes(word,yellow)
+
+        green_letters = WordleTypes(word, green)
+
         if len(grey) > 0 and len(yellow) > 0 and len(green) > 0:
-            if not grey_letters(word, grey) and yellow_letters(word, yellow) and green_letters(word, green):
+            if not grey_letters.letter_check and yellow_letters.letter_check and green_letters.green_check:
                 if word not in guesses:
                     print(word)
 
         elif len(grey) > 0 and len(yellow) > 0:
-            if not grey_letters(word, grey) and yellow_letters(word, yellow):
+            if not grey_letters.letter_check and yellow_letters.letter_check:
                 if word not in guesses:
                     print(word)
 
         elif len(yellow) > 0 and len(green) > 0:
-            if yellow_letters(word, yellow) and green_letters(word, green):
+            if yellow_letters.letter_check and green_letters.green_check:
                 if word not in guesses:
                     print(word)
 
         elif len(grey) > 0 and len(green) > 0:
-            if not grey_letters(word, grey) and green_letters(word, green):
+            if not grey_letters.letter_check and green_letters.green_check:
                 if word not in guesses:
                     print(word)
 
         elif len(green) > 0 and len(grey) == 0 and len(yellow) == 0:
-            if green_letters(word,green):
+            if green_letters.green_check:
                 if word not in guesses:
                     print(word)
 
